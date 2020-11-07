2abf5990ae472402cfe9de4a2c5883fba8815a95
Marvin Calvo <mar90jesus@gmail.com>
2020-11-06 21:31:58 -0600

    Se agrego paypal

---
+++ src/main/webapp/app/layouts/service-payment/service-payment.route.ts
@@ -0,0 +1,7 @@
+import { Route } from '@angular/router';
+
+import { ServicePaymentComponent } from './service-payment.component';
+export const servicePaymentRoute: Route = {
+  path: 'service-payment',
+  component: ServicePaymentComponent,
+};
