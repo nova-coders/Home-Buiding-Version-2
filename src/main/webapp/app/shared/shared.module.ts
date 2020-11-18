c730c7b3ff74988ca79bbb387b2d58aa44e6673b
Steven <cmontesa@ucenfotec.ac.cr>
2020-11-17 21:33:37 -0700

    Descargar contratos - Cambios pequeños necesarios

--- src/main/webapp/app/shared/shared.module.ts
+++ src/main/webapp/app/shared/shared.module.ts
@@ -5,10 +5,18 @@ import { AlertComponent } from './alert/alert.component';
 import { AlertErrorComponent } from './alert/alert-error.component';
 import { LoginModalComponent } from './login/login.component';
 import { HasAnyAuthorityDirective } from './auth/has-any-authority.directive';
+import { DownloadButtonComponent } from 'app/global-components/download-button/download-button.component';
 
 @NgModule({
   imports: [HomeBuildingSharedLibsModule],
-  declarations: [FindLanguageFromKeyPipe, AlertComponent, AlertErrorComponent, LoginModalComponent, HasAnyAuthorityDirective],
+  declarations: [
+    FindLanguageFromKeyPipe,
+    AlertComponent,
+    AlertErrorComponent,
+    LoginModalComponent,
+    HasAnyAuthorityDirective,
+    DownloadButtonComponent,
+  ],
   entryComponents: [LoginModalComponent],
   exports: [
     HomeBuildingSharedLibsModule,
@@ -17,6 +25,7 @@ import { HasAnyAuthorityDirective } from './auth/has-any-authority.directive';
     AlertErrorComponent,
     LoginModalComponent,
     HasAnyAuthorityDirective,
+    DownloadButtonComponent,
   ],
 })
 export class HomeBuildingSharedModule {}
