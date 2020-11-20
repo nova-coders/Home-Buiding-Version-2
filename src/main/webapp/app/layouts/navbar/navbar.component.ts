8526d97f79dd4043215d377d57428fff1864acbd
Steven <cmontesa@ucenfotec.ac.cr>
2020-11-20 00:02:23 -0700

    Mejora en navbar de la aplicacion

--- src/main/webapp/app/layouts/navbar/navbar.component.ts
+++ src/main/webapp/app/layouts/navbar/navbar.component.ts
@@ -9,6 +9,10 @@ import { AccountService } from 'app/core/auth/account.service';
 import { LoginModalService } from 'app/core/login/login-modal.service';
 import { LoginService } from 'app/core/login/login.service';
 import { ProfileService } from 'app/layouts/profiles/profile.service';
+import { Subscription } from 'rxjs';
+import { Account } from 'app/core/user/account.model';
+import { ServicePaymentService } from 'app/service-payment/service-payment.service';
+import { UserAccount } from 'app/shared/model/user-account.model';
 
 @Component({
   selector: 'jhi-navbar',
@@ -16,12 +20,15 @@ import { ProfileService } from 'app/layouts/profiles/profile.service';
   styleUrls: ['navbar.scss'],
 })
 export class NavbarComponent implements OnInit {
+  authSubscription?: Subscription;
   inProduction?: boolean;
   isNavbarCollapsed = true;
   languages = LANGUAGES;
   swaggerEnabled?: boolean;
   version: string;
   header: null;
+  account: Account | null = null;
+  userAccount: UserAccount;
 
   constructor(
     private loginService: LoginService,
@@ -30,12 +37,24 @@ export class NavbarComponent implements OnInit {
     private accountService: AccountService,
     private loginModalService: LoginModalService,
     private profileService: ProfileService,
-    private router: Router
+    private router: Router,
+    private servicePaymentService: ServicePaymentService
   ) {
     this.version = VERSION ? (VERSION.toLowerCase().startsWith('v') ? VERSION : 'v' + VERSION) : '';
+    this.userAccount = new UserAccount();
   }
 
   ngOnInit(): void {
+    this.authSubscription = this.accountService.getAuthenticationState().subscribe(account => {
+      this.account = account;
+      if (this.isAuthenticated()) {
+        this.servicePaymentService.getUserAcoount().subscribe(response => {
+          this.userAccount = <UserAccount>response.body;
+          console.log(this.userAccount);
+        });
+      }
+    });
+
     this.profileService.getProfileInfo().subscribe(profileInfo => {
       this.inProduction = profileInfo.inProduction;
       this.swaggerEnabled = profileInfo.swaggerEnabled;
