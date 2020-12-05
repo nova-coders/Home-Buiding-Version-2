defbf7aca4cb9b7effeeac36a9227a5e9c03a81b
Dennis Granados Ly <dennisly1995@gmail.com>
2020-12-04 19:43:59 -0600

    1. Trabajando en jalar los documentos en listar subastas.

--- src/main/webapp/app/listusersales/list-user-sales.component.ts
+++ src/main/webapp/app/listusersales/list-user-sales.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { UserAccount } from 'app/shared/model/user-account.model';
 import { ServicePaymentService } from 'app/service-payment/service-payment.service';
 import { CustomPropertyService } from 'app/global-services/custom-property.service';
-import { IProperty, Property } from 'app/shared/model/property.model';
+import { Property } from 'app/shared/model/property.model';
 import { Router } from '@angular/router';
 import { SeeAuctionService } from 'app/see-auction/see-auction.service';
 import { CustomOfferService } from 'app/global-services/custom-offer.service';
-import { IOffer, Offer } from 'app/shared/model/offer.model';
-import { HttpResponse } from '@angular/common/http';
+import { Offer } from 'app/shared/model/offer.model';
+import { DocumentService } from 'app/entities/document/document.service';
+import { Document, IDocument } from 'app/shared/model/document.model';
 
 @Component({
   selector: 'jhi-listusersales',
@@ -15,6 +16,7 @@ import { HttpResponse } from '@angular/common/http';
   styleUrls: ['./list-user-sales.component.scss'],
 })
 export class ListUserSalesComponent implements OnInit {
+  myDocuments: Document[] = [];
   myProperties: Property[] = [];
   userAccount?: UserAccount;
   startPage = 1;
@@ -26,6 +28,7 @@ export class ListUserSalesComponent implements OnInit {
     private customPropertyService: CustomPropertyService,
     private seeAuctionService: SeeAuctionService,
     private customOfferService: CustomOfferService,
+    private documentsService: DocumentService,
     private router: Router
   ) {
     this.myProperties = [];
@@ -54,7 +57,23 @@ export class ListUserSalesComponent implements OnInit {
                 this.myProperties[ind].sale!.offers!.push(this.tempOffer);
               }
             });
+            //In this section we place the documents.
+            this.myProperties[ind].documents! = [];
+            this.documentsService.findByProperty(this.myProperties[ind].id!).subscribe(dataD => {
+              console.log('This is the dataD: ');
+              console.log(dataD);
+              /*var tempDocNumber: IDocument | null;
+              tempDocNumber = dataD.body;
+              if (tempDocNumber != null) {
+                this.tempOffer = new Document();
+                this.tempOffer = tempDocNumber;
+                this.myProperties[ind].documents! = [];
+                this.myProperties[ind].documents!.push(this.tempOffer);
+              }*/
+            });
           }
+          console.log('These are the properties after the for loop.');
+          console.log(this.myProperties);
         });
       } else {
         console.log('No user is logged on.');
@@ -64,5 +83,9 @@ export class ListUserSalesComponent implements OnInit {
 
   routetoSaleView(id: number | undefined): void {
     this.router.navigate(['/see-auction/', id]);
+  }
+
+  routetoDocument(id: number | undefined): void {
+    this.router.navigate(['/document/', id]);
   }
 }
