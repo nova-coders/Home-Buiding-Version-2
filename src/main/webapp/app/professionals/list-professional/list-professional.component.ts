7839c55635a4989fa30785852ebd12de12789dc4
Denilson Vargas Ruiz <dvargasr@orbitacr.net>
2020-12-07 10:06:49 -0600

    Filtrar profesionales (1/2)
    Se agrega el filtrar profesionales por tipo de profesión

--- src/main/webapp/app/professionals/list-professional/list-professional.component.ts
+++ src/main/webapp/app/professionals/list-professional/list-professional.component.ts
@@ -12,7 +12,11 @@ import { UserAccount } from 'app/shared/model/user-account.model';
 })
 export class ListProfessionalComponent implements OnInit {
   public proffesionalList: UserAccount[] = [];
+  public proffesionalListFull: UserAccount[] = [];
+  public filteringMode: boolean = false;
+  public userFound: boolean = false;
   public initPage: number = 1;
+  public currentProffesionalFilter: number = -1;
   constructor(public profService: ProfessionalProfileUserService, private route: ActivatedRoute, public userService: UserAccountService) {}
 
   ngOnInit(): void {
@@ -20,7 +24,28 @@ export class ListProfessionalComponent implements OnInit {
       window.scroll(0, 0);
       this.profService.findAll().subscribe(res => {
         this.proffesionalList = res.body as UserAccount[];
+        this.proffesionalListFull = res.body as UserAccount[];
+        this.userFound = true;
       });
     });
+  }
+  searchByName(): void {
+    console.log('Calling event, yes sir.');
+  }
+  filterProfessionType(): void {
+    this.filteringMode = true;
+    if (this.currentProffesionalFilter == -1) {
+      this.proffesionalList = this.proffesionalListFull;
+    } else {
+      var tempList = [];
+      for (var i = 0; i < this.proffesionalListFull.length; i++) {
+        if (this.proffesionalListFull[i].professionalProfileUser?.profesionalType == this.currentProffesionalFilter) {
+          tempList.push(this.proffesionalListFull[i]);
+        }
+      }
+      console.log('Filtered list');
+      console.log(tempList);
+      this.proffesionalList = tempList;
+    }
   }
 }
