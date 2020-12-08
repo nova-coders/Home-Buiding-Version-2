02521bc3c7b3e8cc206827cbad40f029443afb8f
Steven <cmontesa@ucenfotec.ac.cr>
2020-12-07 17:54:33 -0700

    Generar documento de subasta - 90% completo

--- src/main/webapp/app/contract/contract.component.ts
+++ src/main/webapp/app/contract/contract.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { IUserAccount, UserAccount } from 'app/shared/model/user-account.model';
 import { ActivatedRoute } from '@angular/router';
 import { Document } from 'app/shared/model/document.model';
@@ -12,8 +12,7 @@ import { NotificationService } from 'app/entities/notification/notification.serv
 import { Notification } from 'app/shared/model/notification.model';
 import { NotificationType } from 'app/shared/model/enumerations/notification-type.model';
 import * as moment from 'moment';
-import { User } from 'app/core/user/user.model';
-import { not } from 'rxjs/internal-compatibility';
+import { jsPDF } from 'jspdf';
 
 @Component({
   selector: 'jhi-contract',
@@ -21,6 +20,7 @@ import { not } from 'rxjs/internal-compatibility';
   styleUrls: ['./contract.component.scss'],
 })
 export class ContractComponent implements OnInit, OnDestroy {
+  @ViewChild('contentSale') element: ElementRef | undefined;
   salesContract = false;
   contractId: number | null = 0;
 
@@ -35,8 +35,8 @@ export class ContractComponent implements OnInit, OnDestroy {
   disabledBuyer: boolean;
 
   /* Hold the picture of the signature for each user */
-  public sellerSignaturePicture: any;
-  public buyerSignaturePicture: any;
+  sellerSignaturePicture: any;
+  buyerSignaturePicture: any;
 
   success: boolean;
   error: boolean;
@@ -178,5 +178,131 @@ export class ContractComponent implements OnInit, OnDestroy {
     }
 
     return notification;
+  }
+
+  downloadPdf(): void {
+    const content = this.element?.nativeElement;
+
+    if (content != null) {
+      const doc = new jsPDF();
+      let x = 15;
+      let y = 20;
+      doc.setFontSize(22);
+      doc.text('HomeBuilding - Términos del contrato', x, y);
+
+      doc.setFontSize(16);
+      y = this.addSpacing(y);
+      doc.text('Contrato de subasta', x, y);
+
+      //Paragraphs 1
+      doc.setFontSize(11);
+      y = this.addSpacing(y);
+      doc.text(
+        'Comparecen los señores ' +
+          this.contract.buyer?.user?.firstName +
+          ' ' +
+          this.contract.buyer?.user?.lastName +
+          ', mayor, portador de la cédula de identidad número ' +
+          this.contract.buyer?.identification,
+        x,
+        y
+      );
+      y = this.addLineSpacing(y);
+      doc.text(
+        'y ' +
+          this.contract.seller?.user?.firstName +
+          ' ' +
+          this.contract.seller?.user?.lastName +
+          ', mayor, portador de la cédula de identidad número ' +
+          this.contract.seller?.identification +
+          ' y DICEN: Que el',
+        x,
+        y
+      );
+      y = this.addLineSpacing(y);
+      doc.text('primero, en adelante conocido como: EL PROPIETARIO y el segundo en adelante conocido como: EL', x, y);
+      y = this.addLineSpacing(y);
+      doc.text('COMPRADOR, han convenido en acordar los términos siguientes, como normas para la promesa', x, y);
+      y = this.addLineSpacing(y);
+      doc.text('de compraventa a ser realizada con las siguientes cláusulas:', x, y);
+
+      //Paragraphs 2
+      y = this.addSpacing(y);
+      doc.text('Se declara el propietario como legítimo dueño del inmueble presente en este contrato, inscrito en el', x, y);
+      y = this.addLineSpacing(y);
+      doc.text(
+        'Registro Público de la propiedad, situado en ' +
+          this.contract.property?.canton?.province?.name +
+          ' ' +
+          this.contract.property?.canton?.name +
+          'tiene de área ' +
+          this.contract.property?.areaSquareMeters +
+          ' metros cuadrados',
+        x,
+        y
+      );
+      y = this.addLineSpacing(y);
+      doc.text('y tiene dimensiones de ' + this.contract.property?.landSquareMeters + ' metros cuadrados.', x, y);
+
+      //Paragraphs 3
+      y = this.addSpacing(y);
+      doc.text('Por medio del presente acuerdo, EL PROPIETARIO promete la transmisión futura del INMUEBLE, a', x, y);
+      y = this.addLineSpacing(y);
+      doc.text('favor del COMPRADOR, siendo esta última quien lo adquirirá para sí misma, en las condiciones en que', x, y);
+      y = this.addLineSpacing(y);
+      doc.text('actualmente se encuentra, con todos sus usos, mejoras y todo cuanto de hecho y por derecho correspondan.', x, y);
+
+      //Paragraphs 4
+      y = this.addSpacing(y);
+      doc.text('Las PARTES convienen que el precio de la presente operación será de ' + this.bestOffer + ' colones.', x, y);
+      y = this.addLineSpacing(y);
+      doc.text('Las PARTES señalan que en este mismo acuerdo El COMPRADOR liquida totalmente el precio señalado.', x, y);
+
+      //Paragraphs 5
+      y = this.addSpacing(y);
+      doc.text('LAS PARTES reconocen y aceptan que este contrato y sus adicionales constituyen a un acuerdo total', x, y);
+      y = this.addLineSpacing(y);
+      doc.text('entre ellos, por lo que desde el momento de su firma dejan sin efecto cualquier acuerdo o negociación', x, y);
+      y = this.addLineSpacing(y);
+      doc.text('previa, prevaleciendo lo dispuesto en este instrumento respecto, de cualquier otro, hasta en tanto se realice', x, y);
+      y = this.addLineSpacing(y);
+      doc.text('la firma de la escritura o del contrato de compraventa.', x, y);
+
+      //Paragraphs 6
+      y = this.addSpacing(y);
+      doc.text('LAS PARTES convienen que el presente contrato es de caráter privado y, por lo tanto, se abstendrán de', x, y);
+      y = this.addLineSpacing(y);
+      doc.text('comunicar a un tercero su contenido o hacerlo público, así como tampoco podrán divulgar, compartir, o ', x, y);
+      y = this.addLineSpacing(y);
+      doc.text('hacer mal uso de los datos aportados por las PARTES para celebrar el contrato, no obstante, ', x, y);
+      y = this.addLineSpacing(y);
+      doc.text('prevalecerán todos los derechos y obligaciones de LAS PARTES, y por tanto podrán ejercitarse todas', x, y);
+      y = this.addLineSpacing(y);
+      doc.text('las acciones legales que se deriven del mismo conforme a la Ley.', x, y);
+
+      //Images Signatures
+      y = this.addSpacing(y);
+      y = this.addSpacing(y);
+      y = this.addSpacing(y);
+      doc.setLineWidth(1.5);
+
+      doc.addImage(this.contract.seller?.signaturePicture, 'JPEG', x + 10, y, 50, 20);
+      doc.line(x + 10, y + 20, x + 60, y + 20);
+      doc.text('Firma del propietario', x + 10, y + 26);
+
+      doc.addImage(this.contract.buyer?.signaturePicture, 'JPEG', x + 100, y, 50, 20);
+      doc.line(x + 100, y + 20, x + 160, y + 20);
+      doc.text('Firma del comprador', x + 100, y + 26);
+
+      doc.save('Contrato.pdf');
+    }
+  }
+
+  addLineSpacing(pos: number): number {
+    return pos + 6;
+  }
+
+  addSpacing(pos: number): number {
+    return pos + 12;
   }
 }
