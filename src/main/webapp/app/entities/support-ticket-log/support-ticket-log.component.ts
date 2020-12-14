2783213a0f8da0c9d4dcc5d93a07b957ecebe676
Dennis Granados Ly <dennisly1995@gmail.com>
2020-12-13 20:00:18 -0600

    Solucionar tiquete de soporte.
    1. Se agrega POST de logs.

--- src/main/webapp/app/entities/support-ticket-log/support-ticket-log.component.ts
+++ src/main/webapp/app/entities/support-ticket-log/support-ticket-log.component.ts
@@ -2,14 +2,14 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { JhiEventManager } from 'ng-jhipster';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-
-import { ISupportTicketLog } from 'app/shared/model/support-ticket-log.model';
+import * as moment from 'moment/moment';
+import { ISupportTicketLog, SupportTicketLog } from 'app/shared/model/support-ticket-log.model';
 import { SupportTicketLogService } from './support-ticket-log.service';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { SupportTicket } from 'app/shared/model/support-ticket.model';
 import { SupportTicketService } from 'app/entities/support-ticket/support-ticket.service';
-import { UserAccount } from 'app/shared/model/user-account.model';
 import { User } from 'app/core/user/user.model';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'jhi-support-ticket-log',
@@ -18,13 +18,20 @@ import { User } from 'app/core/user/user.model';
 })
 export class SupportTicketLogComponent implements OnInit, OnDestroy {
   supportTicketLogs?: ISupportTicketLog[];
+  supportTicketLog: any;
   eventSubscriber?: Subscription;
   ticketNumber = -1;
   ticketSelected: any;
   client: any;
   ticketResolved = false;
 
+  annotationsForm = this.fb.group({
+    troubleshooting_commentary: [],
+    next_step_commentary: [],
+  });
+
   constructor(
+    private fb: FormBuilder,
     protected supportTicketLogService: SupportTicketLogService,
     private supportTicketService: SupportTicketService,
     protected eventManager: JhiEventManager,
@@ -35,6 +42,7 @@ export class SupportTicketLogComponent implements OnInit, OnDestroy {
     this.ticketNumber = -1;
     this.ticketSelected = new SupportTicket();
     this.client = new User();
+    this.supportTicketLog = new SupportTicketLog();
   }
 
   ngOnInit(): void {
@@ -47,11 +55,25 @@ export class SupportTicketLogComponent implements OnInit, OnDestroy {
         console.log(this.client);
       });
     });
+    this.annotationsForm.patchValue({
+      troubleshooting_commentary: '',
+      next_step_commentary: '',
+    });
   }
 
   ngOnDestroy(): void {
     if (this.eventSubscriber) {
       this.eventManager.destroy(this.eventSubscriber);
     }
+  }
+
+  registerLog(): void {
+    this.supportTicketLog.creationDate = moment();
+    this.supportTicketLog.troubleshooting_commentary = this.annotationsForm.get(['troubleshooting_commentary'])!.value;
+    this.supportTicketLog.troubleshooting_commentary = this.annotationsForm.get(['next_step_commentary'])!.value;
+    console.log(this.supportTicketLog);
+    this.supportTicketLogService.create(this.supportTicketLog).subscribe(data => {
+      console.log(data);
+    });
   }
 }
